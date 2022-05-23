9f0bb50bc10513a9ab59b2bcfc217fbb9cae6a32
ANURAG-BCA <106073489+ANURAG-BCA@users.noreply.github.com>
2022-05-23 16:01:19 +0530

    Add files via upload
    change nothing

---
+++ circle.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+float circle,area,circumfrence;
+printf("enter the value of circle");
+scanf("%f",&circle);
+
+printf("enter the value of area of %fand circumfrence of%f");
+getch(0);
+	
+} 
